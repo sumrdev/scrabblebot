79d9ce66d81ca4bfc56b9c1c4366e91425185c03
Marius Nielsen <mawn@itu.dk>
2024-05-05 14:59:37 +0200

    coolness

--- ScrabbleTemplate/Program.fs
+++ ScrabbleTemplate/Program.fs
@@ -25,7 +25,7 @@ let main argv =
     System.Console.Clear()
 
 //    let board        = ScrabbleUtil.StandardBoard.standardBoard ()
-    let board      = ScrabbleUtil.HoleBoard.holeBoard ()
+    let board      = ScrabbleUtil.InfiniteBoard.infiniteBoard()
 
 //    let board      = ScrabbleUtil.RandomBoard.randomBoard ()
 //    let board      = ScrabbleUtil.RandomBoard.randomBoardSeed (Some 42)
